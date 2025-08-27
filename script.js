b89f1152cccffbc9a2cd26c475438a37de6c960e
Murali Nadipinti <138752441+murali3633@users.noreply.github.com>
2025-08-27 21:31:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ volume = wrapper.querySelector(".word i"),
 infoText = wrapper.querySelector(".info-text"),
 synonyms = wrapper.querySelector(".synonyms .list"),
 removeIcon = wrapper.querySelector(".search span");
-let audio;
+let audio; 
 
 function data(result, word){
     if(result.title){
