6f3f4de9dd33380a5e5d7ebbad075bace2a0b639
Murali Nadipinti <138752441+murali3633@users.noreply.github.com>
2026-04-04 22:19:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const wrapper = document.querySelector(".wrapper"),
 searchInput = wrapper.querySelector("input"),
-volume = wrapper.querySelector(".word i"),
+volume = wrapper.querySelector(".word i"), 
 infoText = wrapper.querySelector(".info-text"), 
 synonyms = wrapper.querySelector(".synonyms .list"),
 removeIcon = wrapper.querySelector(".search span");  
