3561913af722901aebb6f9d31a0a097029b1c0d2
Murali Nadipinti <138752441+murali3633@users.noreply.github.com>
2026-04-09 21:57:17 +0530

    Refactor script.js for improved readability

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ searchInput = wrapper.querySelector("input"),
 volume = wrapper.querySelector(".word i"), 
 infoText = wrapper.querySelector(".info-text"), 
 synonyms = wrapper.querySelector(".synonyms .list"),
-removeIcon = wrapper.querySelector(".search span");  
+removeIcon = wrapper.querySelector(".search span");   
 let audio; 
 function data(result, word){ 
     if(result.title){
